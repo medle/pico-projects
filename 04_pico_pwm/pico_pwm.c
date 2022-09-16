84017b7bcef2ba7ca5749afa892c7e2aa3fbd893
SL <s.larionov@ek.karelia.ru>
2022-09-16 17:21:00 +0300

    PWM example working

--- 04_pico_pwm/pico_pwm.c
+++ 04_pico_pwm/pico_pwm.c
@@ -1,5 +1,4 @@
 
-
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
@@ -31,29 +30,63 @@ int main()
     }
 }
 
+static uint16_t choose_pwm_top_and_divider(
+    uint periods_per_second, bool dual_slope, uint *divider_ptr)
+{
+    const uint max_top = 0xFFFF;
+    const int max_divider = 256;
+    uint32_t clocks_per_second = clock_get_hz(clk_sys);
+
+    uint tops_per_second = dual_slope ? 
+        (2 * periods_per_second) : periods_per_second;
+
+    uint divider = 1;
+    for (; divider <= max_divider; divider++) {
+        // minus one cycle by RP2040 datasheet
+        uint top = clocks_per_second / (divider * tops_per_second) - 1;
+        if (top <= max_top) {
+            *divider_ptr = divider;
+            return top;  
+        }     
+    }
+
+    *divider_ptr = max_divider; 
+    return max_top;
+}
+
 static void configure_pwm(uint gpio)
 {
     uint hz = 8300;
     float duty = 0.25;
 
     gpio_set_function(gpio, GPIO_FUNC_PWM);
-    // Figure out which slice we just connected to the LED pin
+    gpio_set_function(gpio + 1, GPIO_FUNC_PWM);
+
+    // figure out which slice we just connected to the pin
     uint slice_num = pwm_gpio_to_slice_num(gpio);
 
-    // Get some sensible defaults for the slice configuration. By default, the
-    // counter is allowed to wrap over its maximum range (0 to 2**16-1)
+    // dual-slope operation
+    const bool dual_slope = false;
     pwm_config config = pwm_get_default_config();
-
-    // set period [0,count_top] inclusive
-    uint count_top = 10000; //clk_sys / hz;
+    uint divider;
+    uint16_t count_top = choose_pwm_top_and_divider(hz, dual_slope, &divider);
+    pwm_config_set_phase_correct(&config, dual_slope);
     pwm_config_set_wrap(&config, count_top);
+    pwm_config_set_clkdiv_int(&config, divider);
 
-    // Load the configuration into our PWM slice, and set it running.
-    pwm_init(slice_num, &config, false);
+    // channel A is not inverted, channel B is inverted 
+    pwm_config_set_output_polarity(&config, false, true);
 
+    // load into slice but not run
+    pwm_init(slice_num, &config, false); 
+
+    // set PWM compare values for both A/B channels 
     uint high_cycles = (uint)(count_top * duty);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, high_cycles);
+    pwm_set_both_levels(slice_num, high_cycles, high_cycles);
 
+    pwm_set_counter(slice_num, 0);
+
+    // start the counter
     pwm_set_enabled(slice_num, true);
 }
 
