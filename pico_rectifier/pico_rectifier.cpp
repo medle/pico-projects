41e768e6450c9a2297d7c89e8ed98c28b51c7069
SL <s.larionov@ek.karelia.ru>
2024-04-16 21:52:24 +0300

    button debouncing v1

--- pico_rectifier/pico_rectifier.cpp
+++ pico_rectifier/pico_rectifier.cpp
@@ -2,133 +2,129 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+
 #include "hardware/i2c.h"
-#include "hardware/clocks.h"
-#include "hardware/pwm.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
 
 #include <ssd1306.h>
 #include <ShapeRenderer.h>
 #include <TextRenderer.h>
 
-#define LCD_I2C_PORT i2c0
-#define LCD_I2C_ADDRESS 0x3C
-#define LCD_I2C_BAUDRATE 1000000
-#define LCD_I2C_SDA_PIN 20
-#define LCD_I2C_SCL_PIN 21
+#include "easy_pwm.h"
+#include "easy_eeprom.h"
+#include "easy_buttons.h"
+#include "board_config.h"
 
-static void enable_pwm(uint gpio, uint hz, float duty);
-static void disable_pwm(uint gpio);
+static void init_display();
+static void enable_relay(uint relay_gpio, bool on);
+static void core1_entry(); 
 
-int main() {
+static pico_ssd1306::SSD1306 *display;
+
+static uint n = 0;
+
+static void button_callback(uint gpio, bool on)
+{
+    if (on) n += 1;
+    char buf[50];
+    sprintf(buf, "#%d %s", n, (on ? "ON" : "OFF"));  
+    pico_ssd1306::fillRect(display, 0, 16, 128, 32, pico_ssd1306::WriteMode::SUBTRACT);
+    pico_ssd1306::drawText(display, font_12x16, buf, 0, 16);
+    display->sendBuffer(); 
+}
+
+int main() 
+{
     stdio_init_all();
+    adc_init();
+    init_display(); 
+    easy_eeprom_init();
 
-    // initialise GPIO (Green LED connected to pin 25)
-    gpio_init(25);
-    gpio_set_dir(25, GPIO_OUT);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    i2c_init(LCD_I2C_PORT, LCD_I2C_BAUDRATE); 
-    gpio_set_function(LCD_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(LCD_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(LCD_I2C_SDA_PIN);
-    gpio_pull_up(LCD_I2C_SCL_PIN);
+    gpio_init(BUTTON_1_PIN);
+    gpio_set_dir(BUTTON_1_PIN, GPIO_IN);
 
-    // Create a new display object
-    pico_ssd1306::SSD1306 display = pico_ssd1306::SSD1306(
-        LCD_I2C_PORT, LCD_I2C_ADDRESS, pico_ssd1306::Size::W128xH32);
+    enable_relay(RELAY1_PIN, true); 
 
-    // let the LCD time to initialize itself     
-    sleep_ms(300);
+    for(int j=0; j<2; j++) { 
+      easy_pwm_enable(SOUND_GEN_PIN, SOUND_GEN_HZ, 0.5);  
+      sleep_ms(100);
+      easy_pwm_disable(SOUND_GEN_PIN);
+      sleep_ms(100);
+    }
 
-    pico_ssd1306::drawText(&display, font_12x16, "Rectifier", 0, 0);
-    display.sendBuffer(); //Send buffer to device and show on screen
+    enable_relay(RELAY2_PIN, true); 
 
-    enable_pwm(14, 6000, 0.5);  
-    sleep_ms(500);
-    disable_pwm(14);
+    multicore_launch_core1(core1_entry);
 
     // Main Loop 
     int i = 0;
-    while(true) {
-        gpio_put(25, 1); // Set pin 25 to high
-        sleep_ms(500); 
 
-        gpio_put(25, 0); // Set pin 25 to low
-        sleep_ms(500); 
+    //easy_eeprom_read_bytes(0, (uint8_t *)&i, sizeof(int));
+    easy_buttons_register(BUTTON_1_PIN, button_callback);
+
+    while(true) {
+        gpio_put(LED_PIN, 1); 
+        easy_buttons_sleep_ms(500); 
+
+        gpio_put(LED_PIN, 0); 
+        easy_buttons_sleep_ms(500); 
+
+        // clear line and print a new text  
+        pico_ssd1306::fillRect(display, 0, 0, 128, 16, pico_ssd1306::WriteMode::SUBTRACT);
 
         char buf[50];
-        sprintf(buf, "N=%d\n", ++i);  
- 
-        // clear line and print a new text  
-        pico_ssd1306::fillRect(&display, 0, 16, 128, 32, pico_ssd1306::WriteMode::SUBTRACT);
-        pico_ssd1306::drawText(&display, font_12x16, buf, 0, 16);
-        display.sendBuffer(); 
+        sprintf(buf, "#%d", ++i);  
+        pico_ssd1306::drawText(display, font_12x16, buf, 0, 0);
+        display->sendBuffer(); 
+
+        //easy_eeprom_write_bytes(0, (uint8_t *)&i, sizeof(int));
     }
 
     return 0;
 }
 
-static uint16_t choose_pwm_top_and_divider(
-    uint periods_per_second, bool dual_slope, uint *divider_ptr)
+//
+// Core1 procedure runs in parallel to the main loop running in Core0.
+//
+static void core1_entry() 
 {
-    const uint max_top = 0xFFFF;
-    const int max_divider = 256;
-    uint32_t clocks_per_second = clock_get_hz(clk_sys);
-
-    uint tops_per_second = dual_slope ? 
-        (2 * periods_per_second) : periods_per_second;
-
-    uint divider = 1;
-    for (; divider <= max_divider; divider++) {
-        // minus one cycle by RP2040 datasheet
-        uint top = clocks_per_second / (divider * tops_per_second) - 1;
-        if (top <= max_top) {
-            *divider_ptr = divider;
-            return top;  
-        }     
+    while (1) {
+        tight_loop_contents();
     }
-
-    *divider_ptr = max_divider; 
-    return max_top;
 }
 
-static void enable_pwm(uint gpio, uint hz, float duty)
+static void init_display()
 {
-    gpio_set_function(gpio, GPIO_FUNC_PWM);
-    gpio_set_function(gpio + 1, GPIO_FUNC_PWM);
+    i2c_init(DISPLAY_I2C_PORT, DISPLAY_I2C_BAUDRATE); 
+    gpio_set_function(DISPLAY_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(DISPLAY_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(DISPLAY_I2C_SDA_PIN);
+    gpio_pull_up(DISPLAY_I2C_SCL_PIN);
 
-    // figure out which slice we just connected to the pin
-    uint slice_num = pwm_gpio_to_slice_num(gpio);
+    // create a new display object
+    display = new pico_ssd1306::SSD1306(
+        DISPLAY_I2C_PORT, DISPLAY_I2C_ADDRESS, pico_ssd1306::Size::W128xH32);
 
-    // dual-slope operation
-    const bool dual_slope = false;
-    pwm_config config = pwm_get_default_config();
-    uint divider;
-    uint16_t count_top = choose_pwm_top_and_divider(hz, dual_slope, &divider);
-    pwm_config_set_phase_correct(&config, dual_slope);
-    pwm_config_set_wrap(&config, count_top);
-    pwm_config_set_clkdiv_int(&config, divider);
+    // let the LCD time to initialize itself     
+    sleep_ms(200);
 
-    // channel A is not inverted, channel B is inverted 
-    pwm_config_set_output_polarity(&config, false, true);
+    // set flipped orientation    
+    display->setOrientation(false);  
 
-    // load into slice but not run
-    pwm_init(slice_num, &config, false); 
-
-    // set PWM compare values for both A/B channels 
-    uint high_cycles = (uint)(count_top * duty);
-    pwm_set_both_levels(slice_num, high_cycles, high_cycles);
-
-    pwm_set_counter(slice_num, 0);
-
-    // start the counter
-    pwm_set_enabled(slice_num, true);
+    pico_ssd1306::drawText(display, font_12x16, "Rectifier", 0, 0);
+    display->sendBuffer(); // send buffer to device and show on screen
 }
 
-static void disable_pwm(uint gpio)
+static void enable_relay(uint relay_gpio, bool on)
 {
-    // figure out which slice we just connected to the pin
-    uint slice_num = pwm_gpio_to_slice_num(gpio);
-    pwm_set_enabled(slice_num, false);
-    gpio_set_function(gpio, GPIO_FUNC_NULL);
+    gpio_init(relay_gpio);
+    gpio_set_dir(relay_gpio, GPIO_OUT);
+    gpio_put(relay_gpio, on); 
 }
